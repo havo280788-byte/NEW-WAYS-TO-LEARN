824d2860be545e6530777ae5df7956bc8e9277fb
havo280788-byte <havo.280788@gmail.com>
2026-02-18 14:39:42 +0700

    Add files via upload

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Replace with your actual Firebase project configuration
-// You can find these in the Firebase Console -> Project Settings
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBRvcMvef9xx78Z0flL4wW1mvwodO1JrwA",
+    authDomain: "learningquest-50743.firebaseapp.com",
+    projectId: "learningquest-50743",
+    storageBucket: "learningquest-50743.firebasestorage.app",
+    messagingSenderId: "256079977788",
+    appId: "1:256079977788:web:5e146bc4804d179d895239"
 };
 
 const app = initializeApp(firebaseConfig);
