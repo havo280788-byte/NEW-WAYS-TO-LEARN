c3bc234d79dc8994c03903a309490e14d74ec16b
havo280788-byte <havo.280788@gmail.com>
2026-02-19 13:27:25 +0700

    Add files via upload

--- constants.ts
+++ constants.ts
@@ -136,18 +136,19 @@ export const SOUNDS = {
 };
 
 export const LEARNING_QUEST_PASSAGE = `
-# Apps of the Future
+# APPS OF THE FUTURE
 
 How many times a day do you tap the icon of your favourite social media site or play a game on your smartphone? When your parents were young, apps didn’t even exist, but now we can’t imagine our lives without them. So what will apps be like in the future?
 
 **A new way to learn**
-Apps are being used in education. They are useful because students can use them anytime, anywhere and on any device. They present information in **bite-sized chunks**, which people find easy to understand and remember. Lessons can also be turned into games, making learning fun! Soon, apps will be the new normal.
+
+Apps are being used in education. They are useful because students can use them anytime, anywhere and on any device. They present information in bite-sized chunks, which people find easy to understand and remember. Lessons can also be turned into games, making learning fun! Soon, apps will be the new normal.
 
 **Augmented Reality (AR)**
+
 Inside AR apps, the real world is mixed with the digital one. These apps are downloaded and teachers use them to make learning more interesting. These apps capture students’ attention and help them concentrate and interact with their lessons. Students can experience the material and become more interested in the subject. At the same time, they can explore the topic taught at their own pace.
 
-**Future Impact**
-In the future, apps will be able to do a lot of things for us. Some people think this will have a lot of benefits, others think it will make us lazy or that we will forget how to do things for ourselves. Whatever your opinion on modern technology, one thing is certain: the **apps of the future will change our lives**.
+In the future, apps will be able to do a lot of things for us. Some people think this will have a lot of benefits, others think it will make us lazy or that we will forget how to do things for ourselves. Whatever your opinion on modern technology, one thing is certain: the apps of the future will change our lives.
 `;
 
 export const LEARNING_QUEST_POOL = [
